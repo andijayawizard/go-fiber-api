2889114307ec616a96ff459c7125e0a0f7af7cb9
andijayawizard <andijayawizard@gmail.com>
2026-05-15 22:29:22 +0700

    15may2026

--- database/connection.go
+++ database/connection.go
@@ -14,13 +14,14 @@ var DB *gorm.DB
 func Connect() {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
+	sslmode := os.Getenv("SSL_MODE")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port,
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode,
 	)
 
 	var err error
